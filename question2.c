6dc5e1ea28ae9ac304a5bd39f535bc86960b5b5c
QuamarAbrar <73212122+QuamarAbrar@users.noreply.github.com>
2026-02-09 22:29:49 +0530

    Add program to convert lowercase to uppercase

---
+++ question2.c
@@ -0,0 +1,13 @@
+// lowercase to UPPERCASE
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    char lower;
+    scanf("%c", &lower);
+    char upper = lower - 32;
+    printf("The uppercase of %c is %c", lower, upper);
+    return 0;
+}

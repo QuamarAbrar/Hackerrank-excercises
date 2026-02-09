e19a96b8564256c50d48b1208e0370e391fd35ee
QuamarAbrar <73212122+QuamarAbrar@users.noreply.github.com>
2026-02-09 22:31:57 +0530

    Add program to sum two numbers

---
+++ question3.c
@@ -0,0 +1,13 @@
+// Sum of Two Numbers
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    int num1, num2;
+    scanf("%d %d", &num1, &num2);
+    printf("The sum of %d and %d is %d.", num1, num2, num1 + num2);
+    return 0;
+}

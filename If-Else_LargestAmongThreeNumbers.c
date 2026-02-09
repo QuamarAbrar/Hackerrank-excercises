744c924f322f3dced41a37d102aa693a6c4255a1
QuamarAbrar <73212122+QuamarAbrar@users.noreply.github.com>
2026-02-09 22:55:24 +0530

    Implement program to find largest among three numbers

---
+++ If-Else_LargestAmongThreeNumbers.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    int num1, num2, num3;
+    
+    scanf("%d %d %d", &num1, &num2, &num3);
+    
+    if(num1>num2){
+        if(num1>num3){
+            printf("The largest number is : %d", num1);
+        }
+        else{
+            printf("The largest number is : %d", num3);
+        }
+    }
+    else{
+        if(num2>num3){
+            printf("The largest number is : %d", num2);
+        }
+        else{
+            printf("The largest number is : %d", num3);
+        }
+    }
+    return 0;
+}

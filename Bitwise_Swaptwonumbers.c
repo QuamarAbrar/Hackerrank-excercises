aa022d791cd78fc7a0d4d26b1c7d75cd57b8c2a4
QuamarAbrar <73212122+QuamarAbrar@users.noreply.github.com>
2026-02-09 23:17:55 +0530

    Fix bitwise swap implementation for two numbers

--- Bitwise_Swaptwonumbers.c
+++ Bitwise_Swaptwonumbers.c
@@ -2,13 +2,14 @@
 
 int main() {
 
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */  
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
     int num1, num2;
+    scanf("%d\n%d", &num1, &num2);
     
-    scanf("%d", &num1);
-    scanf("%d", &num2);
+    num1 = num1^num2;
+    num2 = num1^num2;
+    num1 = num1^num2;
     
-    printf("%d\n%d", num2, num1);
-    
+    printf("%d\n%d", num1, num2);
     return 0;
 }

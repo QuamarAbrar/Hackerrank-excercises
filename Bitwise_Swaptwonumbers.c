2bba7a945e2ed6ddf1b16258b664c70de14854bf
QuamarAbrar <73212122+QuamarAbrar@users.noreply.github.com>
2026-02-09 23:15:46 +0530

    Implement bitwise swap of two numbers

---
+++ Bitwise_Swaptwonumbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */  
+    int num1, num2;
+    
+    scanf("%d", &num1);
+    scanf("%d", &num2);
+    
+    printf("%d\n%d", num2, num1);
+    
+    return 0;
+}

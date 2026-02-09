5fb807507ede6d4ce330fa7f2b5bb8e222e6db02
QuamarAbrar <73212122+QuamarAbrar@users.noreply.github.com>
2026-02-09 22:50:02 +0530

    Add Bitwise Even or Odd checker

---
+++ Bitwise_EvenorOdd.c
@@ -0,0 +1,17 @@
+// Bitwise_Even or Odd
+#include <stdio.h>
+
+int main() {
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    int num;
+    scanf("%d", &num);
+    
+    if ((num & 1)==0){
+        printf("Even");
+    }
+    else{
+        printf("Odd");
+    }
+    return 0;
+}

d31668741a8baa111ca418e2a7d9c59f5a087608
QuamarAbrar <73212122+QuamarAbrar@users.noreply.github.com>
2026-02-09 23:02:16 +0530

    Implement leap year check in If-Else_LeapYear.c
    Corrected leap year logic for better accuracy.

---
+++ If-Else_LeapYear.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */ 
+    int year;
+    scanf("%d", &year);
+    
+    if(year>0){
+        if((year%400)==0){
+                printf("Yes, %d is a leap year.", year);
+            }
+        else if(year%400==1){
+                printf("No, %d is not a leap year.", year);
+            }
+        else if((year%4)==0 && year%100!=0){
+            printf("Yes, %d is a leap year.", year);
+        }
+        else{
+            printf("No, %d is not a leap year.", year);
+        }
+    }
+    else{
+        printf("The given year %d is Invalid year.", year);
+    }
+    return 0;
+}

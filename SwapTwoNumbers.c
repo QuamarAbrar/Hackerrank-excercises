98dfd4c4db53923cc044035111c0e25c76a024fb
QuamarAbrar <73212122+QuamarAbrar@users.noreply.github.com>
2026-02-09 22:53:52 +0530

    Implement SwapTwoNumbers program

---
+++ SwapTwoNumbers.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    int a, b;
+    scanf("%d %d", &a, &b);
+    
+    printf("a = %d\nb = %d", b, a);
+    return 0;
+}
